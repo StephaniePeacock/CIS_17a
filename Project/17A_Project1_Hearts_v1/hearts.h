4f4fe43e30742fb0833ed8cc72cbbf6ab9f84cd5
StephaniePeacock <127582902+StephaniePeacock@users.noreply.github.com>
2023-10-28 23:32:21 -0700

    player card validation complete
    need to fix npc card choice.

--- Project/17A_Project1_Hearts_v1/hearts.h
+++ Project/17A_Project1_Hearts_v1/hearts.h
@@ -45,7 +45,7 @@ void playCard(Player *, Show *, int);   //Determine which card to play (Stooge)
 void played(Player &,Show &);           //Print out the cards played (nested inside playCard)
 void set(Player &, Show &);             //Set the player's choice to card value (nested inside playCard)
 void trick(Player *);                   //Score the trick
-void unset(Player &, Show *);           //Set choice back & remove played card from hand (nested inside trick)
+void unset(Player &, Show &);           //Set choice back & remove played card from hand (nested inside trick)
 void count();                           //Counts how many tricks have been played
 
 #endif /* HEARTS_H */
