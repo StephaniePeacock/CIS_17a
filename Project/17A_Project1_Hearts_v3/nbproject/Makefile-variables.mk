efad2388c468324af0c904f2b35eb6874ffc2210
StephaniePeacock <127582902+StephaniePeacock@users.noreply.github.com>
2023-11-12 19:01:58 -0800

    Added file writing

---
+++ Project/17A_Project1_Hearts_v3/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=17a_project1_hearts_v3
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/17a_project1_hearts_v3
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=17aproject1heartsv3.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/17aproject1heartsv3.tar
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=17a_project1_hearts_v3
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/17a_project1_hearts_v3
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=17aproject1heartsv3.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/17aproject1heartsv3.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
